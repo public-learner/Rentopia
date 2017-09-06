18dd4b30c92e6742d93d864f66bab3abf7eabe1c
public-learner <sweetjustin0717@gmail.com>
2017-09-05 20:46:46 -0500

    password strength indicator set up. Connecting PUT request in progress

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
 import { setEditedProfileInfo } from '../../actions/setEditedProfileInfo';
+import ReactPasswordStrength from '../PasswordStrength.jsx'
 
 class UserProfile extends Component {
 	constructor() {
@@ -9,28 +10,69 @@ class UserProfile extends Component {
 
 		this.state = {
 			editing: false,
-			confirmCount: 0
+			confirmCount: 0,
+			passLength: 0,
+			newPassword: '',
+			editingPassword: false
 		}
+		this.handleEditPassword = this.handleEditPassword.bind(this)
+	}
+
+	handlePassword(password) {
+		this.setState({newPassword: password})
+	}
+
+	handleEditPassword() {
+		this.setState({editingPassword: !this.state.editingPassword})
 	}
 
 	handleSubmit(event) {
 		event.preventDefault()
-
+		console.log('password', this.state.password)
 		let name = event.target.name.value 
 		let email = event.target.email.value 
 		let confirm = event.target.confirm.value
+		let currentPassword = event.target.currentPassword.value
+		let confirmPassword = event.target.confirmPassword.value
 
 		if (email === confirm && email !== '') {
-			this.props.setEditedProfileInfo({name: name, email: email}, this.props.userId)
+			var profileObj = {}
+			if (this.state.editingPassword) {
+				if (this.state.newPassword === confirmPassword) {
+					profileObj = {
+						user_name: name,
+						email: email,
+						user_password: currentPassword,
+						new_password: this.state.newPassword
+					}
+			  } else {
+			  	alert('Passwords do not match')
+			  }
+			} else {
+				profileObj = {
+					name: name,
+					email: email,
+				}
+			}
+
+			this.props.setEditedProfileInfo(profileObj, this.props.userId)
+			  .then((response) => {
+			  	console.log('response from profile edit', response)
+			  })
+
 			event.target.name.value = ''
 			event.target.email.value = ''
 			event.target.confirm.value = ''
+			event.target.confirmPassword.value = ''
+			event.target.currentPassword.value = ''
+
 			this.setState({
 				confirmCount: 0,
-				editing: false
+				editing: false,
+				password: ''
 			})
 		} else {
-			alert("those emails don't match. You done messed up boiieee!!!")
+			alert("You done messed up boiieee!!!\nMake sure all fields are filled in properly")
 		}
 	}
 
@@ -43,11 +85,51 @@ class UserProfile extends Component {
   	}
 	}
 
+
+	changeCallback(state) {
+	  this.setState({ passLength: state.password.length });
+	}
+
+	clear() {
+	  this.refs.passComponent.clear();
+	}
+
+	editPassword() {
+		const inputProps = {
+		  placeholder: "Try a password...",
+		  id: "inputPassword",
+		  autoFocus: true,
+		  className: 'another-input-prop-class-name',
+		};
+
+		return (
+			<div>
+				<label>Current Password</label>
+				<input type="password" className="form-control" name="currentPassword"/>
+				<br/>
+				<label>New Password</label>
+				<ReactPasswordStrength
+				   ref="passComponent"
+				   minLength={6}
+				   inputProps={inputProps}
+				   changeCallback={this.changeCallback.bind(this)}
+				   handlePassword={this.handlePassword.bind(this)}
+				 />
+				 <br/>
+				 <label>Confirm Password</label>
+				 <input type="password" className="form-control" name="confirmPassword"/>
+			 </div>
+		)
+	}
+
 	editForm() {
 		return (
 			<div className="editForm">
 				<form onSubmit={this.handleSubmit.bind(this)}>
-				  <label>Name</label>
+				  <div>
+					  <label>Name</label>
+					  <a style={{float: "right"}} onClick={this.handleEditPassword}> Edit Password </a>
+				  </div>
 				  <input type="text" className="form-control" name="name" defaultValue={this.props.name}/>
 					<br/>
 				  <label>Email address</label>
@@ -56,6 +138,8 @@ class UserProfile extends Component {
 					<br/>
 				  <label>Confirm email</label>
 				  <input id="confirmEmail" type="email" className="form-control" name="confirm" defaultValue={this.props.email}/>
+					<br/>
+					{this.state.editingPassword && this.editPassword()}
 					<br/>
 					<button className="paymentForm"> Save Changes </button>
 				</form>
