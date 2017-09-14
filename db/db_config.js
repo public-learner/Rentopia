9216094ad597fe38ad10c679849c0f1c1219095b
public-learner <sweetjustin0717@gmail.com>
2017-09-14 16:24:22 -0500

    commit for rebase

--- db/db_config.js
+++ db/db_config.js
@@ -2,6 +2,7 @@ const { Pool, Client } = require('pg')
 
 // pools will use environment variables
 // for connection information
+console.log(process.env.PGDATABASE)
 const pool = new Pool()
    
 module.exports = {
